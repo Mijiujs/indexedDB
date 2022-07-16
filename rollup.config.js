91daafa22e6e0fa0dc0c1ec7bca9dcaa78ae111c
mijiu <mijiu@webuy.ai>
2022-07-16 15:10:31 +0800

    init config

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser'; // 压缩
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'lib/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'lib/index.es.js',
+      format: 'es',
+    },
+    {
+      file: 'lib/index.umd.js',
+      format: 'umd',
+      name: 'IDB',
+    }
+  ],
+  plugins: [
+    terser(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**', // 只编译我们的源代码
+    })
+  ],
+}
